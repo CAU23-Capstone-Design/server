be979cdbf9a8281d56653c16d6a3469663810947
Myeongseokkang <kms00129@naver.com>
2023-03-21 17:06:25 +0900

    fetch: findByCode 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -105,6 +105,48 @@ router.get('/',async (req, res, next) => {
     }
 });
 
+/**
+ * @swagger
+ * /users/findByCode:
+ *  get:
+ *      summary: 코드를 기준으로 사용자 정보 반환
+ *      tags:
+ *       - User
+ *      parameters:
+ *       - in: query
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: 사용자 코드
+ *      responses:
+ *          200: 
+ *              description: 코드에 일치하는 사용자 정보
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: 사용자를 찾을 수 없음
+ */
+router.get('/findByCode', async (req, res, next) => {
+  try {
+      const { code } = req.query;
+      const user = await User.findOne({ code: code });
+
+      if (!user) {
+          res.status(404).json({ "error": "user not found" });
+          return;
+      }
+
+      res.json(user);
+  } catch (err) {
+      console.error(err);
+      next(err);
+  }
+});
+
+
 
 /**
  * @swagger
