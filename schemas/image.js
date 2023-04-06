ae1b2aa93c6d461c740a1fe0296828addd6570f2
Myeongseokkang <kms00129@naver.com>
2023-04-06 21:09:07 +0900

    fetch : 이미지 업로드 기능 추가

--- schemas/image.js
+++ schemas/image.js
@@ -1,17 +1,7 @@
-// models/image.js
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-
 const imageSchema = new Schema({
-  image_id: {
-    type: String,
-    required: true,
-  },
-  local_id: {
-    type: String,
-    required: true,
-  },
   couple_id: {
     type: String,
     required: true,
@@ -20,17 +10,29 @@ const imageSchema = new Schema({
     type: String,
     required: true,
   },
-  original_name: {
+  local_id: {
     type: String,
     required: true,
   },
-  server_file_name: {
+  image_url: {
     type: String,
     required: true,
   },
-  upload_date: {
-    type: Date,
-    default: Date.now,
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
   },
 });
 
